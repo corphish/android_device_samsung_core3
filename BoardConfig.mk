a698cf3d94b99c3e166b94c51efd65563ae0d520
corphish <d97.avinaba@gmail.com>
2015-12-11 22:48:58 +0530

    BoardConfig: Re-order arch, kernel and partition info

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,22 +1,40 @@
-USE_CAMERA_STUB := true
+#
+# Copyright (C) 2015 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# inherit from the proprietary version
--include vendor/samsung/core33g/BoardConfigVendor.mk
 
+# Arch
 TARGET_ARCH := arm
-TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := sc8830
+TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a7
+TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
-TARGET_BOOTLOADER_BOARD_NAME := core33g
+TARGET_BOARD_PLATFORM := sc8830
 
+# Kernel
+TARGET_PREBUILT_KERNEL := device/samsung/core33g/kernel
 
-BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8
-BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_PAGESIZE := 2048
+# Partition
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1258291200
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 6094323712
+BOARD_FLASH_BLOCK_SIZE := 131072
+TARGET_USERIMAGES_USE_EXT4 := true
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
@@ -48,6 +66,6 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 6094323712
 BOARD_FLASH_BLOCK_SIZE := 131072
 TARGET_USERIMAGES_USE_EXT4 := true
 
-TARGET_PREBUILT_KERNEL := device/samsung/core33g/kernel
+
 
 BOARD_HAS_NO_SELECT_BUTTON := true
