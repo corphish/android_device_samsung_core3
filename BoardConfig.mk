617c89a5eea1d2525797532ae96ecf1f06f325d6
corphish <d97.avinaba@gmail.com>
2015-12-11 22:56:56 +0530

    BoardConfig: Add WiFi configs And cleanup.........

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,18 +41,20 @@ TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM:=true
 
-# GPS
-TARGET_SPECIFIC_HEADER_PATH := device/samsung/kylepro/include
-
-# CMHW
-BOARD_HARDWARE_CLASS := hardware/samsung/cmhw/ device/samsung/kylepro/cmhw/
-
 # Connectivity - Wi-Fi
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 WPA_SUPPLICANT_VERSION      := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER        := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE           := bcmdhd
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA     := "/etc/wifi/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_P2P     := "/etc/wifi/fw_bcmdhd_p2p.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/etc/wifi/fw_bcmdhd_apsta.bin"
+#WIFI_DRIVER_MODULE_NAME     := "bcmdhd"                            //WiFi built-in kernel. No module, so we hash out them
+#WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/bcmdhd.ko"
 
-# Wi-Fi Tethering
-BOARD_HAVE_SAMSUNG_WIFI := true
 
 # Hardware rendering
 TARGET_BOARD_PLATFORM_GPU := ARM Mali-400
